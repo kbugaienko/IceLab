7a1a502f5f33a9b9efeed44f08adcf91b0d5f233
Khrystyna <kr.bugaienko@gmail.com>
2022-09-06 13:59:37 +0200

    Create interactive search bar
    * Add styles
    * Add js functions

--- js/main.js
+++ js/main.js
@@ -5,7 +5,24 @@ $(function(){
     slidesToShow: 1,
     slidesToScroll: 1,
   });
+  
   $('.menu__btn').on('click', function(){
     $('.header__list').toggleClass('header__list--active');
+  });
+
+  $('.header__search-input').on('focus', function() {
+    $('.header__search-input').addClass('header__search-input-open');
+    $('.header__search-btn').addClass('header__search-btn-show');
+  });
+  
+  $('.header__search-input').on('blur', function() {
+    if ($('.header__search-input').val() === '') {
+      $('.header__search-input').removeClass('header__search-input-open');
+      $('.header__search-btn').removeClass('header__search-btn-show');
+    }
+  });
+
+  $('.header__search-btn').on('click', function() {
+    $('.header__search-input').val('').focus();
   });
 });
